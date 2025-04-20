8dc6d506716f61568fb7a1f96cb12c539580a8fc
lucena564 <victor_alb98@hotmail.com>
2025-04-19 23:27:49 -0300

    menager com os dois agentes criados, falta atualizar o botão para olhar os movimentos

--- menager.py
+++ menager.py
@@ -9,12 +9,13 @@ from openai import OpenAI
 # Carregar as variáveis do arquivo .env
 load_dotenv()
 
-class OpenAIAgent:
-    def __init__(self, api_key: str, model="gpt-4o-mini"):
-        """Inicializa a classe com a chave da API da OpenAI e o modelo."""
-        self.api_key = api_key
-        self.model = model
-        self.instruction = None
+class Menager:
+    def __init__(self, api_key_openai: str, api_key_deepseek: str, model_openai="gpt-4o-mini", model_deepseek="deepseek-chat"):
+        """Inicializa a classe com a chave da API da OpenAI e o model_openaio."""
+        self.api_key_openai = api_key_openai
+        self.api_key_deepseek = api_key_deepseek
+        self.model_openai = model_openai
+        self.model_deepseek = model_deepseek
         self.context = ''
         self.last_move = None  
 
@@ -22,19 +23,13 @@ class OpenAIAgent:
         """Define o último movimento realizado pelo agente."""
         self.last_move = move
 
-    def load_instruction(self, instruction: str):
-        """Carrega a instrução inicial do agente."""
-        self.instruction = instruction
-        self.context = instruction  # Inicializa o contexto com a instrução
-
-    def interact_with_agent(self, prompt: str, context="A partida ainda não começou, escolha o seu primeiro movimento"):
+    def interact_with_OpenAI(self, prompt: str, context="A partida ainda não começou, escolha o seu primeiro movimento"):
         self.context = context
-        """Interage com o modelo da OpenAI com base no contexto e no prompt dado."""
-        CHAVE_DA_OPEN_AI=self.api_key
+        CHAVE_DA_OPEN_AI=self.api_key_openai
         client = OpenAI(api_key=CHAVE_DA_OPEN_AI)
         try:
             resposta = client.chat.completions.create(
-                model=self.model,
+                model=self.model_openai,
                 messages=[
                     {"role": "system", "content": f"""Você está jogando xadrez. Para cada movimento, você deve responder com o movimento em notação algébrica. Você pode usar a notação "e4" para mover um peão para e4, "Nf3" para mover um cavalo para f3, etc. Não forneça explicações ou justificativas. Além disso, você irá saber o estado atual do tabuleiro para decidir a sua jogada.
 
@@ -44,6 +39,27 @@ Responda **apenas** assim: {{ "move": "<movimentação_da_peça>" }}."""},
                     {"role": "user", "content": prompt}
                 ],
                 temperature=0.1
+                )
+            return json.loads(resposta.choices[0].message.content)
+        except Exception as e:
+            return f"Erro ao obter resposta: {str(e)}"
+        
+    def interact_with_DeepSeek(self, prompt: str, context="Você é o jogador com as peças pretas."):
+        self.context = context
+        CHAVE_DO_DEEPSEEK=self.api_key_deepseek
+        client = OpenAI(api_key=CHAVE_DO_DEEPSEEK, base_url="https://api.deepseek.com")
+        try:
+            resposta = client.chat.completions.create(
+                model=self.model_deepseek,
+                messages=[
+                    {"role": "system", "content": f"""Você está jogando xadrez. Para cada movimento, você deve responder com o movimento em notação algébrica. Você pode usar a notação "e4" para mover um peão para e4, "Nf3" para mover um cavalo para f3, etc. Não forneça explicações ou justificativas. Além disso, você irá saber o estado atual do tabuleiro para decidir a sua jogada.
+
+Estado atual do tabuleiro: {self.context}
+
+Responda **apenas** assim: {{ "move": "<movimentação_da_peça>" }}."""},
+                    {"role": "user", "content": prompt}
+                ],
+                stream=False
                 )
             return json.loads(resposta.choices[0].message.content)
         except Exception as e:
